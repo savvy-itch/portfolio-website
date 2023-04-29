ba6f621a7492ae6f0293a126a0fd6257c435ccb3
Mik-Halo <sprone3@gmail.com>
2023-04-29 14:15:59 +0300

    responsive navbar finished

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ const heading = document.getElementById('name-heading');
 const subHeading = document.getElementById('occupation-heading');
 const skillsGrid = document.querySelector('.skills-grid');
 const icons = document.querySelectorAll('.icon');
-const navLinks = document.querySelectorAll('.nav-links a');
+const links = document.querySelector('.nav-links');
+const liLinks = links.querySelectorAll('li');
+const navLinks = links.querySelectorAll('a');
+const navToggle = document.querySelector('.nav-toggle');
+const showLinks = document.querySelector('.show-links');
 const headerTxt = "Hello, I'm Mykhailo";
 const subheaderTxt = "Front-End Developer";
-let typeSpeed = 90;
+let typeSpeed = 95;
 let cursorSpeed = 500;
 let showCursor = true;
 let i = 0;
 let y = 0;
 
+// typing animation
 document.addEventListener('DOMContentLoaded', typeWords);
+
 function typeWords() {
   if (i < headerTxt.length) {
     heading.innerHTML += headerTxt.charAt(i);
@@ -43,7 +49,6 @@ icons.forEach(icon => {
 });
 
 function displayTooltip(e) {
-  console.log(e);
   if (e.target.classList.contains('icon')) {
     const tooltip = document.createElement('div');
     tooltip.classList.add('tooltip');
@@ -58,6 +63,7 @@ function hideTooltip(e) {
   e.target.parentElement.removeChild(tooltip)
 }
 
+// smooth scrolling
 navLinks.forEach(link => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
@@ -67,3 +73,29 @@ navLinks.forEach(link => {
       });
   })
 })
+
+
+
+// mobile navbar
+window.addEventListener('resize', handleLinksDisplay);
+
+navToggle.addEventListener('click', function() {
+  links.classList.toggle('show-links');
+  handleLinksDisplay();
+});
+
+function handleLinksDisplay() {
+  if (window.innerWidth <= 768) {
+    navToggle.style.transform = 'rotate(90deg)';
+    if (links.classList.contains('show-links')) {
+      let linksHeight = 0;
+      liLinks.forEach(li => linksHeight += (li.getBoundingClientRect().height + 7));
+      links.style.height = linksHeight + 'px';
+    } else {
+      navToggle.style.transform = 'rotate(0deg)';
+      links.style.height = '0';
+    }
+  } else {
+    links.style.height = 'auto';
+  }
+}
